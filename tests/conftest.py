8f36bd5a2add2cc65cc1bcfc80f29fb2375f1bcd
Luk Pe <luk.pe@outlook.com>
2021-03-24 12:48:51 +0100

    run local instance of OpenMRS if url code == 200

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,28 @@
+import urllib.request, urllib.error
 import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from webdrivermanager import ChromeDriverManager, GeckoDriverManager
 from utils.excel_driver import ExcelDriver
-
 from utils.test_base import TestBase
 
 
 def pytest_addoption(parser):
     default = TestBase.get_config("driver", "default")
     parser.addoption("--browser", action="store", default=default)
+
+
+def get_url():
+    url_local = TestBase.get_config("test", "url_local")
+    url = TestBase.get_config("test", "url")
+    try:
+        status_code = urllib.request.urlopen(url_local).getcode()
+        if status_code == 200:
+            return url_local
+        else:
+            return url
+    except urllib.error.HTTPError:
+        return url
 
 
 @pytest.fixture(scope="class")
@@ -25,7 +38,7 @@ def setup(request):
         driver = webdriver.Firefox()
 
     driver.maximize_window()
-    driver.get(TestBase.get_config("test", "url"))
+    driver.get(get_url())
     driver.delete_all_cookies()
     wait = WebDriverWait(driver, TestBase.get_config("driver", "wait"))
     request.cls.driver = driver
