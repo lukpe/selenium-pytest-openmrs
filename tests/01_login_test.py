d9c8fdaf19b95d274ab486cc46815fef9294d300
Luk Pe <luk.pe@outlook.com>
2021-03-26 10:33:45 +0100

    logging login tests

--- tests/01_login_test.py
+++ tests/01_login_test.py
@@ -18,17 +18,19 @@ class TestLoggingIn(TestBase):
         log = self.get_logger()
         login_page = LoginPage(self.driver)
         home_page = HomePage(self.driver)
-        log.info("Log In to the Inpatient Ward")
+        log.info("Log In to the Inpatient Ward: %s", f"{user_login}/{user_password}")
         login_page.log_in(
             user=user_login, password=user_password, location="Inpatient Ward"
         )
         if is_correct:
+            log.info("Expect user to log in")
             assert "Home" in home_page.get_title()
             assert (
                 "Logged in as Super User (admin) at Inpatient Ward."
                 in home_page.get_header()
             )
         elif not is_correct:
+            log.info("Expect error message")
             assert "Login" in login_page.get_title()
             assert (
                 "Invalid username/password. Please try again."
